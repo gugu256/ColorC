c4aa697d822263d9277c6ef77355a5b60ac6cd23
gugu256 <gugu256@mail.com>
2023-09-16 11:30:22 +0200

    added demos

--- demo.c
+++ demo.c
@@ -2,8 +2,24 @@
 #include "colorc.h"
 
 int main() {
-    printf("%sREDTEXT%s\n", FORE_RED, RESET_ALL);
-    printf("%sBLUEGREEN%s\n", FORE_BLUE, RESET_ALL);
-    
+    printf("%sBlack text%s\n", FORE_BLACK, RESET_ALL);
+    printf("%sRed text%s\n", FORE_RED, RESET_ALL);
+    printf("%sGreen text%s\n", FORE_GREEN, RESET_ALL);
+    printf("%sYellow text%s\n", FORE_YELLOW, RESET_ALL);
+    printf("%sBlue text%s\n", FORE_BLUE, RESET_ALL);
+    printf("%sMagenta text%s\n", FORE_MAGENTA, RESET_ALL);
+    printf("%sCyan text%s\n", FORE_CYAN, RESET_ALL);  
+    printf("%sWhite text%s\n\n", FORE_WHITE, RESET_ALL);
+
+    printf("%sBlack background%s\n", BACK_BLACK, RESET_ALL);
+    printf("%sRed background%s\n", BACK_RED, RESET_ALL);
+    printf("%sGreen background%s\n", BACK_GREEN, RESET_ALL);
+    printf("%sYellow background%s\n", BACK_YELLOW, RESET_ALL);
+    printf("%sBlue background%s\n", BACK_BLUE, RESET_ALL);
+    printf("%sMagenta background%s\n", BACK_MAGENTA, RESET_ALL);
+    printf("%sCyan background%s\n", BACK_CYAN, RESET_ALL);  
+    printf("%sWhite background%s\n\n", BACK_WHITE, RESET_ALL);
+
+    printf("%s %sYou can also get crazy with it%s%s%s!!%s\n", BACK_GREEN, FORE_RED, RESET_ALL, BACK_BLUE, FORE_YELLOW, RESET_ALL);
     return 0;
 }
