88f82a325930ebcc4cc340096d38c051b3faf389
gugu256 <gugu256@mail.com>
2023-09-16 11:30:03 +0200

    background and forground finished

--- colorc.h
+++ colorc.h
@@ -1,1 +1,21 @@
 #include <stdio.h>
+
+const char RESET_ALL[] = "\033[0m";
+
+const char FORE_BLACK[] = "\033[30m";
+const char FORE_RED[] = "\033[31m";
+const char FORE_GREEN[] = "\033[32m";
+const char FORE_YELLOW[] = "\033[33m";
+const char FORE_BLUE[] = "\033[34m";
+const char FORE_MAGENTA[] = "\033[35m";
+const char FORE_CYAN[] = "\033[36m";
+const char FORE_WHITE[] = "\033[37m";
+
+const char BACK_BLACK[] = "\033[40m";
+const char BACK_RED[] = "\033[41m";
+const char BACK_GREEN[] = "\033[42m";
+const char BACK_YELLOW[] = "\033[43m";
+const char BACK_BLUE[] = "\033[44m";
+const char BACK_MAGENTA[] = "\033[45m";
+const char BACK_CYAN[] = "\033[46m";
+const char BACK_WHITE[] = "\033[47m";
